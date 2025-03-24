dffa70de6bb3daf1bc71c47ceeb1e499df4559f0
Roman Kuzmin <ghelatin@mail.ru>
2025-03-24 11:49:00 +0300

    fix: расширенный лог фронта перемещен в filter package

--- src/main/java/ru/skypro/homework/filter/BasicAuthCorsFilter.java
+++ src/main/java/ru/skypro/homework/filter/BasicAuthCorsFilter.java
@@ -3,7 +3,6 @@ package ru.skypro.homework.filter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
